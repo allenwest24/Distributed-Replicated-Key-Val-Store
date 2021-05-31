c8b7fc70999736980e4c0fdbd0acbcf1533d34d1
Allen West <58156836+allenwest24@users.noreply.github.com>
2021-05-31 14:39:46 -0400

    Initial commit with the beginning of the global variables translated.

---
+++ rework_in_rust.rs
@@ -0,0 +1,15 @@
+// This file is the Rust re-work of the Python Distributed KVS algorithm I designed loosely based
+// on Raft.
+
+// Imports.
+use std::collections::HashMap;
+
+// Global Variables.
+
+// Storage methods being used must be Key-Value oriented.
+let mut log = HashMap::new();
+let mut kvs = HashMap::new();
+// A node enters as a follower by default.
+let mut state = "follower";
+// A randomly chosen integer in a given range to differentiate tolerance times.
+let elect_to : i32 = 0; //TODO: Make this a random value in a range.
